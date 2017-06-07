0c0e6ef97a211a5d5b3c09d6ec4408fc04373186
DESKTOP-OBRTUKM\UC <koemponleu@gmail.com>
2017-06-08 00:01:45 +0700

    then all

---
+++ SpringTest1/src/main/java/com/SpringTest1/demo/model/Test1.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.SpringTest1.demo.model;
+
+/**
+ *
+ * @author UC
+ */
+public class Test1 {
+    private int i;
+    private String j;
+
+    public Test1(int i, String j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public String getJ() {
+        return j;
+    }
+
+    public void setJ(String j) {
+        this.j = j;
+    }
+}
